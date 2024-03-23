346d6f71c49b50ed055f2900768f5ae20fef9ed3
naufalammar <naufalammarz04@gmail.com>
2024-03-23 08:44:28 +0700

    JumlahTigaAngka

--- modul5_1302220052/Program.cs
+++ modul5_1302220052/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+namespace modul5_1302220052
+{
+    class Program
+    {
+
+        public class Penjumlahan
+        {
+            public static T JumlahTigaAngka<T>(T angka1, T angka2, T angka3) where T : IAdditionOperators<T, T, T>
+            {
+
+
+                return angka1 + angka2 + angka3;
+            }
+            private static void Main(string[] args)
+            {
+                //1302220052
+
+                Console.WriteLine("Hasil Penjumlahan adalah 37", JumlahTigaAngka<float>(13, 02, 22));
+            }
+        }
+
+
+    }
+}
